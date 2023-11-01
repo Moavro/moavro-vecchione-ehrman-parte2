14bd4e4c209820a131e7b9182dfa36537e631f8a
NicolasEhrman <nehrman@udesa.edu.ar>
2023-11-01 14:41:29 -0300

    register

---
+++ parte2/src/screens/Register.js
@@ -0,0 +1,79 @@
+import react, { Component } from 'react';
+//import { auth, db } from '../../firebase/config';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+
+class Register extends Component{
+    constructor(){
+        super()
+            this.state={
+                email: '',
+                password: '',
+                username: ''
+
+            }
+        
+    }
+
+    register(){
+
+    }
+
+    render(){
+        return(
+            <View style={styles.form}>
+                <Text >Register</Text>
+                <TextInput 
+                style={styles.input}
+                placeholder='email'
+                keyboardType='email-address'
+                onChangeText={text => this.setState({email:text})}
+                value={this.state.email}/>
+                <TextInput
+                style={styles.input}
+                placeholder='password'
+                keyboardType='default'
+                secureTextEntry={true}
+                onChangeText={text => this.setState({password:text})}
+                value={this.state.password}/>
+                <TextInput
+                style={styles.input}
+                placeholder='username'
+                keyboardType='default'
+                onChangeText={text => this.setState({username:text})}
+                value={this.state.username}/>
+                <TouchableOpacity style={styles.boton} onPress={()=> this.register}>
+                    <Text>Registrarse</Text>
+
+                </TouchableOpacity>
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    form:{paddingHorizontal: 550,
+    paddingVertical: 200},
+    input:{
+        height:50,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: 'green',
+        borderRadius: 40,
+        marginVertical:10,
+        marginHorizontal: 10
+    },
+    boton: {
+        height: 50,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderColor: 'green',
+        borderRadius: 40,
+        backgroundColor: 'green'
+        
+    }
+})
+
+export default Register
+
